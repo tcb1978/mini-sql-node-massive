bb2c114fc3734d606b2d0e200563af34b8ac65ea
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-14 14:12:00 -0700

    weekthree thursday mini projectmini-sql-node-massive

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const massive = require('massive');
+const port = process.env.PORT;
 const app = express();
+const controller = require('./controller.js');
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db);
+
+    // db.new_planes().then(planes => { 
+    //     console.log(planes) 
+    // }).catch(err => console.error(err))
+
+    // db.get_planes().then(planes => {
+    //     console.log(planes)
+    // }).catch(err => console.error(err))
+
+}).catch(err=>console.error(err))
+
+app.get('/api/airplanes', controller.getPlanes)
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
-const port = process.env.PORT || 3000
+
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
 
